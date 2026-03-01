9ea46baea70340420337d1c65082cf5530c7fc17
Fawzia Tasnim <fawzit20@gmail.com>
2026-03-01 23:39:48 +0600

    Initialize particles.js with configuration settings

--- particles.js
+++ particles.js
@@ -1,1 +1,21 @@
-
+particlesJS("particles-js", {
+    "particles": {
+        "number": {"value": 70},
+        "color": {"value": "#00eaff"},
+        "shape": {"type": "circle"},
+        "opacity": {"value": 0.6},
+        "size": {"value": 3},
+        "line_linked": {"enable": true, "distance": 120, "color": "#00eaff", "opacity": 0.4, "width": 1},
+        "move": {"enable": true, "speed": 2, "out_mode": "bounce"}
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {"enable": true, "mode": "grab"},
+            "onclick": {"enable": true, "mode": "push"}
+        },
+        "modes": {
+            "grab": {"distance": 150},
+            "push": {"particles_nb": 4}
+        }
+    }
+});
